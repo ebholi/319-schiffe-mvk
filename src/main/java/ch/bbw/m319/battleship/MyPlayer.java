0331469380718ad42772c9915d6184ef0e4f2d76
ebholi <uliver@gmx.ch>
2026-06-11 10:18:27 +0200

    Implemented Randomized Aim in MyPlayer.java

--- src/main/java/ch/bbw/m319/battleship/MyPlayer.java
+++ src/main/java/ch/bbw/m319/battleship/MyPlayer.java
@@ -7,20 +7,35 @@ import ch.bbw.m319.battleship.api.ShipPosition;
 
 public class MyPlayer implements BattleshipPlayer {
 
-	public static void main(String[] args) {
-		// let it play against itself
-		BattleshipArena.playOnce(new MyPlayer(), new MyPlayer());
-	}
+    public static void main(String[] args) {
+        // let it play against itself
+        BattleshipArena.playOnce(new MyPlayer(), new MyPlayer());
+    }
 
-	@Override
-	public ShipPosition placeYourShip() {
-		// TODO: replace this implementation: always top-left is not that good...
-		return new ShipPosition(BattleshipField.A1, BattleshipField.A2);
-	}
+    @Override
+    public ShipPosition placeYourShip() {
+        // TODO: replace this implementation: always top-left is not that good...
+        return new ShipPosition(BattleshipField.A1, BattleshipField.A2);
+    }
 
-	@Override
-	public BattleshipField takeAim() {
-		// TODO: replace this implementation: always bottom-right is not that good...
-		return BattleshipField.C3;
-	}
+    @Override
+    public BattleshipField takeAim() {
+        double columnNum = Math.floor(Math.random() * 3);
+        double row = Math.floor(Math.random() * 3) + 1;
+        int rowInt = (int) row;
+
+        String columnLetter;
+
+        if (columnNum == 0) {
+            columnLetter = "A";
+        } else if (columnNum == 1) {
+            columnLetter = "B";
+        } else {
+            columnLetter = "C";
+        }
+
+        String attackField = columnLetter + rowInt;
+        System.out.println(attackField);
+        return BattleshipField.valueOf(attackField);
+    }
 }
